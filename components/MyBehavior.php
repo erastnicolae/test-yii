24eadba3a9224453cd30cefde001bf87b845a306
Nicolae Erast <nicolae.erast@fngcimm.ro>
2017-08-25 14:30:51 +0300

    testing behaviors

--- components/MyBehavior.php
+++ components/MyBehavior.php
@@ -3,20 +3,36 @@
 namespace app\components;
 
 use yii\base\Behavior;
+use yii\db\ActiveRecord;
 
 class MyBehavior extends Behavior {
 	public $prop1;
 	private $_prop2;
 
-	public function getProp2() {
+	public function getProp2()
+	{
 		return $this->_prop2;
 	}
 
-	public function setProp2($value) {
+	public function setProp2($value)
+	{
 		$this->_prop2 = $value;
 	}
 
-	public function foo() {
+	public function events()
+	{
+		return [
+			ActiveRecord::EVENT_BEFORE_VALIDATE => 'beforeValidate',
+			]
+	}
+
+	public function beforeValidate($event)
+	{
+		//...
+	}
+
+	public function foo()
+	{
 		//...
 	}
 }
--- models/Test.php
+++ models/Test.php
@@ -1,1 +1,20 @@
-Test
+<?php
+
+namespace app\models;
+
+use yii\components\MyBehavior;
+use yii\db\ActiveRecord;
+
+class Test extends ActiveRecord
+{
+	public function behaviors()
+	{
+		return [
+			'myBehavior4' => [
+				'class' => MyBehaviour::className(),
+				'prop1' => 'value1',
+				'prop2' => 'value2'
+				]
+		];
+	}
+}
